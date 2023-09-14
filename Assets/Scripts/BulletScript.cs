c7cc8ece0321170d16c84c65cb5fc078ffc14830
MeezHu <93200594+MeezHu@users.noreply.github.com>
2023-09-14 16:52:19 +0200

    Health and death

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     [SerializeField] private HealthBar _healthbar;
+    [SerializeField] GameObject player;
+    //Health health;
+
+
+    void Awake()
+    {
+        //health = player.GetComponent<Health>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Player hit");
+            //health.currentHealth = (health.maxHealth -= 1);
+            Health.currentHealth--;
+            
             Destroy(gameObject);
         }
+
+        
     }
 
+    /*void Update()
+    {
+        
+    }*/
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public int maxHealth = 3;
-    public int currentHealth;
+    public int maxHealth;
+    public static int currentHealth;
+    public Image healthBar;
 
     void Start()
     {
         currentHealth = maxHealth;
-        
+
+    }
+
+    private void Update()
+    {
+        death();
+    }
+
+    public void death()
+    {
+        if(currentHealth <= 0)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 
 }
